54a4caeebc46b30cc830f1bdc52f1fd449f13b59
Gionata Tratzi <tratzigionata@gmail.com>
2024-12-04 16:00:52 +0100

    recognizers

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -2,17 +2,52 @@ type word = char list;;
 
 let charlist_of_string (lst : string) : word =
   lst |> String.to_seq |> List.of_seq;;
-  
-let lang1 w = Str.string_match (Str.regexp "^[01]+$") w 0;;
 
-let lang2 w = Str.string_match (Str.regexp "^0?1*$") w 0;;
+(*lang col pattern matching*) 
+(*  1. `[01]+`
+    2. `0?1*`
+    3. `0[01]*0`
+    4. `0*10*10*`
+    5. `(00|11)+ 
+*)
 
-let lang3 w = Str.string_match (Str.regexp "^0[01]*0$") w 0;;
+(*  1. `[01]+` *)
+let rec lang1 = function
+    [] -> false
+  | hd::tl -> hd = 0 || hd = 1 || lang1 tl
+;;
 
-let lang4 w = Str.string_match (Str.regexp "^0*10*10*$") w 0;;
+(*  2. `0?1*` *)
+let rec lang2 = function
+    [] -> true
+  | 0::l -> not(List.mem 0 l) && lang2 l
+  | 1::l -> not(List.mem 0 l) && lang2 l
+  | _ -> false
+;;  
 
-let lang5 w = Str.string_match (Str.regexp "^\\(00\\|11\\)+$") w 0;;
-    
+(* 3. `0[01]*0` *)
+let rec lang3 = function
+    [] | [1] -> false
+  | 0::l -> (l = [0] || l = [1]) && lang3 l
+  | hd::tl -> if tl = [] then hd = 0 else false
+;;
+
+(* 4. `0*10*10*` *)
+let rec lang4 = function
+    [] -> false
+  | 0::tl
+  | 1::tl ->  if tl <> [] then (tl = [1] || tl = [0]) && lang4 tl else false
+  | hd::tl -> if tl = [] then (hd = 1 || hd = 0) else false
+;;
+
+(* 5. `(00|11)+ *)
+let rec lang5 = function
+    [] -> false
+  | 0::0::l -> true && lang5 l
+  | 1::1::l -> true && lang5 l
+  | _ -> false
+;;
+
 let recognizers = [lang1;lang2;lang3;lang4;lang5];;
                   
 let belongsTo w = List.map (fun f -> f w) recognizers;;
